2034dc6b4b85e91d763e3d50f740d7e596cae5ef
muhammedarshadtp <arshadtp89@gmail.com>
2025-07-10 10:01:29 +0530

    Studied Python lists, tuples, sets, and intro to dictionaries

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python
+# basic under stnting for Python and print Hello world 
+# List in Python
+# Tuples
+# Dictionary
--- main.py
+++ main.py
@@ -1,4 +1,59 @@
 
-print('Hello world')
+# print('Hello world')
 
-print(3 + 3)
+# print(3 + 3)
+
+# x = 10
+
+# print(x + 20)
+
+# print(x+12)
+
+# name = 'youtube'
+# print(len(name))
+
+# fruit =["apple","banana","Mango"]
+# del fruit[1:]
+# print(fruit)
+
+#        Tuples
+
+# num = ("apple")
+# print(num)
+
+#     Dictionary
+
+
+person = {
+    "name":"Arshad",
+    "age":24,
+    "city":"Kannur"
+}
+print(person)
+
+# # Access Value
+# print(person["city"])
+
+# # change value
+# person["age"]=26
+# print(person.get("age"))
+
+# # add key-value pair
+# person["email"] = "arshad@gmail.com"
+# print(person.get("email"))
+
+# # remove key
+# person.pop("city")
+# print(person)
+
+# #check if check key Exits
+# if "name" in person:
+#  print("yes")
+
+#  Dictionary Methods
+
+# print(person.get("age"))
+# print(person.keys())
+# print(person.values())
+# print(person.items())
+# print(person.pop("age"))
