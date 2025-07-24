43c02c29b937e3e473c287b510ef9fada8c51c34
muhammedarshadtp <arshadtp89@gmail.com>
2025-07-24 20:14:59 +0530

    function passing arguments in different ways

--- Function.py
+++ Function.py
@@ -1,11 +1,19 @@
 
-def greet():
-    print("hello World")
+# def greet():
+#     print("hello World")
 
-greet()
+# greet()
 
 
-def add(a,b):
-    c= a*b
-    print(c)
-add(4,5)
+# def add(a,b):
+#     c= a*b
+#     print(c)
+# add(4,5)
+
+#   ---------Keyword variable length arguments---
+
+def person(name,**data):
+    print(name)
+    for i,j in data.items():
+        print(i,j)
+person("arshad",age=34,city="kannur",mob=123456789)    
